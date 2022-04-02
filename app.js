8d42129d0bc782ed2e6ca69b79663e7f88f72a80
git.Ball <georgiballabanov@gmail.com>
2022-04-02 15:49:03 +0300

    Adding complated files.

---
+++ app.js
@@ -0,0 +1,62 @@
+
+
+const toggleButton = document.querySelector("#toggle-btn");
+const navLinks = document.querySelector(".navbar-links");
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+window.addEventListener("scroll", function () {
+  let scroll = document.querySelector(".scrollTop");
+  scroll.classList.toggle("active", window.scrollY > 500);
+});
+
+const topDiv = document
+  .querySelector(".scrollTop")
+  .addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+const widthOutput = document.querySelector("#width");
+
+export function reportWindowSize() {
+  widthOutput.textContent = window.innerWidth;
+  if (widthOutput.textContent < 760) {
+    return gliderDowngrade();
+  } else {
+    return gliderUpdate();
+  }
+}
+
+
+document.body.onresize = reportWindowSize;
+
+reportWindowSize();
+
+
+
+function gliderUpdate() {
+  return new Glider(document.querySelector(".glider"), {
+    slidesToShow: 3,
+    slideToScroll: 2,
+    dots: "#dots",
+    arrows: {
+      prev: ".glider-prev",
+      next: ".glider-next",
+    },
+  });
+}
+function gliderDowngrade() {
+  return new Glider(document.querySelector(".glider"), {
+    slidesToShow: 1,
+    slideToScroll: 1,
+    dots: "#dots",
+    arrows: {
+      prev: ".glider-prev",
+      next: ".glider-next",
+    },
+  });
+}
